a0a4c4b8b04718231de638640ba55ac55df699c7
Alejo-P <marcelo.pinzon@epn.edu.ec>
2024-02-14 23:02:33 -0500

    Commit de vuelta

--- proyecto-3/tarea-DI/src/App.jsx
+++ proyecto-3/tarea-DI/src/App.jsx
@@ -20,6 +20,20 @@ function App() {
 
   return (
     <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo"/>
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
       <h1>Pagina de inicio</h1>
       {/*  Creacion de la pagina web con minimo de contenido*/}
       <p>Parrafo creado en HTML a traves de  React</p>
